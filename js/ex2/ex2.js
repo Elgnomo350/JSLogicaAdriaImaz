b3d54a6ed7e8acc8ebeda9238546322c015eb484
Adria Imaz <adriaimaz03@gmail.com>
2025-11-20 09:30:29 +0100

    ex2

---
+++ js/ex2/ex2.js
@@ -0,0 +1,7 @@
+const x = 888
+const y = 50
+
+// condicion ? expresionSiVerdadero : expresionSiFalso;
+
+x > 10 && x < 50 ? console.log(x + "esta en rango") : console.log("No esta en rango")
+y >= 10 && y <= 50 ? console.log(y + " esta en rango") : console.log("No esta en rango")
