e94f61987a28f655fa367d0fd9795c5425dc7f2a
Adria Imaz <adriaimaz03@gmail.com>
2025-11-20 09:39:37 +0100

    ex3

---
+++ js/ex3/ex3.js
@@ -0,0 +1,13 @@
+let x = 5.99
+
+if (x > 0 && x <= 4.99) {
+  console.log("Suspes")
+} else if (x > 5 && x <= 5.99) {
+  console.log("Sufiecient")
+} else if (x > 6 && x <= 6.99) {
+  console.log("Be")
+} else if (x > 7 && x <= 8.99) {
+  console.log("Notable")
+} else if (x > 9 && x <= 10) {
+  console.log("Excel-lent")
+}
