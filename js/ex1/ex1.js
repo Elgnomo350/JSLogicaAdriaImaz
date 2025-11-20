caae78d3bf55efd6bcef7c25cedc98bdec00d2c1
Adria Imaz <adriaimaz03@gmail.com>
2025-11-20 09:16:30 +0100

    ex1

---
+++ js/ex1/ex1.js
@@ -0,0 +1,13 @@
+let n1 = 21
+
+if (n1 % 3 === 0) {
+  console.log("el numero " + n1 + " Es divisible entre tres")
+} else {
+  console.log("No es divisible entre 3")
+}
+
+if (n1 % 7 === 0) {
+  console.log("el numero " + n1 + " Es divisible entre siete")
+} else {
+  console.log("No es divisible entre 7")
+}
